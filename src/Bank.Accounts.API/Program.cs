aa155dc010683621bd1c020c60c8865fca7f8fa9
Jose Junior <jlfjunior8@gmail.com>
2025-01-04 14:06:57 -0300

    feat: add in-memory database provider to transaction project

--- src/Bank.Accounts.API/Program.cs
+++ src/Bank.Accounts.API/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddScoped<AccountService>();
 
-builder.Services.AddDbContext<Context>(options => options.UseInMemoryDatabase("InMemoryDatabase"));
+builder.Services.AddDbContext<Context>(options => options.UseInMemoryDatabase("AccountsInMemoryDatabase"));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- src/Bank.Transactions.API/Program.cs
+++ src/Bank.Transactions.API/Program.cs
@@ -1,8 +1,11 @@
 using Bank.Transactions.API;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Context>(options => options.UseInMemoryDatabase("TransactionsInMemoryDatabase"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
